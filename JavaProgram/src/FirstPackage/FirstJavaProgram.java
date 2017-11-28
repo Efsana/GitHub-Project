d722f3655dead2a61530573aaf3c7605eb322966
akter <akter@192.168.0.5>
2017-11-28 01:19:48 -0500

    I Change Something

---
+++ JavaProgram/src/FirstPackage/FirstJavaProgram.java
@@ -0,0 +1,21 @@
+package FirstPackage;
+
+public class FirstJavaProgram {
+
+	public static void main(String[] args) 
+	{
+	//System.out.println("My First Program");
+	//System.out.println("Java is a high-level programming language ");
+	int text=100;
+	System.out.println(text);
+	
+	String name="Efsana";
+	System.out.println(name);
+	System.out.println("Name: "+name);
+	
+	boolean flag = false;
+	System.out.println(flag);
+	
+	}
+
+}
